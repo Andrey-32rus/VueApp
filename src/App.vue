c36cd0b08b9c9174370d70450ab74ad0690bbcc8
SimutinAS <s95simutin@gmail.com>
2019-09-29 16:43:01 +0300

    no logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png"><br/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"><br/> -->
     <Currencies/>
   </div>
 </template>
--- src/components/Currencies.vue
+++ src/components/Currencies.vue
@@ -24,6 +24,7 @@
 
 <script>
 import axios from 'axios'
+
 export default {
     name: 'Currencies',
     data(){
