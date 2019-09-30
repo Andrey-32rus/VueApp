a1436eaf4bea15f82f9593c902da07d358d19e5c
SimutinAS <s95simutin@gmail.com>
2019-09-30 12:50:23 +0300

    loading

--- src/components/Currencies.vue
+++ src/components/Currencies.vue
@@ -1,24 +1,30 @@
 <template>
 <div id="Currencies">
-    <button @click="getCurrencies">Обновить</button>
-    <table v-if="this.currencies.length != 0">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Code</th>
-                <th>Name</th>
-                <th>BaseRate</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="cur in currencies" :key="cur.ID">
-                <td>{{cur.ID}}</td>
-                <td>{{cur.CC}}</td>
-                <td>{{cur.NM}}</td>
-                <td>{{cur.BR}}</td>
-            </tr>
-        </tbody>
-    </table>
+    <template v-if="isLoading === true">
+        Loading...
+    </template>
+    <template v-else>
+        <button @click="getCurrencies">Обновить</button>
+        <table v-if="this.currencies.length != 0">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Code</th>
+                    <th>Name</th>
+                    <th>BaseRate</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="cur in currencies" :key="cur.ID">
+                    <td>{{cur.ID}}</td>
+                    <td>{{cur.CC}}</td>
+                    <td>{{cur.NM}}</td>
+                    <td>{{cur.BR}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </template>
+    
 </div>
 </template>
 
@@ -31,24 +37,28 @@ export default {
     },
     data(){
         return {
-            currencies:[]
+            currencies:[],
+            isLoading: true
         };
     },
     methods:{
         getCurrencies(){
+            this.isLoading = true;
             //console.log('getCurrencies');
             axios.get(this.url)
-           .then(response => {
-               if(response.status == 200){
+            .then(response => {
+                if(response.status == 200){
                     this.currencies = response.data;
-               }
-               else{
-                   alert(response.status);
-               }
-               
-           })
-           .catch(error => {
+                }
+                else{
+                    alert(response.status);
+                }
+            })
+            .catch(error => {
                alert(error);
+            })
+            .finally(() => {
+                this.isLoading = false;
             }); 
         }
     },
