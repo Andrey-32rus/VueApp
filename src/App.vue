6c9063054fb2dd8cfa0749a9bd8eee1234f54857
SimutinAS <s95simutin@gmail.com>
2019-10-18 22:20:18 +0300

    боковое меню, пока что так

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"><br/> -->
-    <Currencies url="http://192.168.1.101:9990/api/currencies" @req-time="reqTime"/>
-    <p v-if="requestMilliseconds !== -1">Время выполнения запроса: {{this.requestMilliseconds}}ms</p>
+    <Aside @menu-choice="menuChoice"/>
+    <div class="main">
+      <Currencies v-if="mainElementKey === 'currencies'" url="http://192.168.1.101:9990/api/currencies" @req-time="reqTime"/>
+      <p v-if="requestMilliseconds !== -1">Время выполнения запроса: {{this.requestMilliseconds}}ms</p>
+    </div>
   </div>
 </template>
 
 <script>
 import Currencies from './components/Currencies'
+import Aside from './components/Aside'
 
 export default {
   components: {
-    Currencies
+    Currencies,
+    Aside,
   },
 
   data(){
     return{
       requestMilliseconds: -1,
+      mainElementKey: String,
     };
   },
 
@@ -24,10 +30,26 @@ export default {
     reqTime(milliseconds){
       //console.log(milliseconds);
       this.requestMilliseconds = milliseconds;
+    },
+    menuChoice(key){
+      this.mainElementKey = key;
     }
   }
 }
 </script>
+
+<style scoped>
+.main{
+  width: 85vw;
+}
+
+@media screen and (max-width:479px) {
+    /* Target portrait smartphones */
+    .main{
+      width: 75vw;
+    }
+}
+</style>
 
 <style>
 #app {
@@ -36,6 +58,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex
   /* margin-top: 60px; */
 }
 
---
+++ src/components/Aside.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="Aside">
+      <p>Меню</p>
+      <ul>
+          <li @click="menuChoice('currencies')">Currencies</li>
+          <li @click="menuChoice('none')">None</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data(){
+    return{
+      
+    };
+  },
+
+  methods:{
+    menuChoice(key){
+        this.$emit('menu-choice', key);
+    }
+  }
+}
+</script>
+
+<style scoped>
+#Aside{
+    width: 15vw;
+    border: 1px solid black;
+}
+
+ul{
+    margin-top: 1.5rem;
+    padding: 0;
+}
+li{
+    list-style-type: none;
+    cursor: pointer;
+    margin-top: 0.7rem;
+}
+p{
+    font-weight: bold;
+}
+
+@media screen and (max-width:479px) {
+    /* Target portrait smartphones */
+    #Aside{
+        width: 25vw;
+    }
+}
+
+</style>
